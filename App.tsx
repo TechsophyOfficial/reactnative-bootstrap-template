b62c0d4ecbbafb4dbdefe58f6f2169613c178a72
sravan.v <sravan.v@techsophy.com>
2024-08-29 09:22:22 +0530

    updated to 0.75.2

--- App.tsx
+++ App.tsx
@@ -57,7 +57,6 @@ const App = () => {
       <AppSettingsContext.Provider value={AppContextObject}>
         <StatusBar barStyle={'dark-content'} backgroundColor={'#ffffff'} />
         <PaperProvider theme={mainTheme}>
-          ``
           <SafeAreaView style={backgroundStyle}>
             <ReactNativeKeycloakProvider
               authClient={keycloak}
